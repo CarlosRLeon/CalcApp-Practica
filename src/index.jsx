652bf535d4855923a927d90356c788012b346d19
Kenichi_PC <crojelleonn@gmail.com>
2021-01-29 12:52:11 -0600

    Mi primer commits

---
+++ src/index.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+//importar el componente
+import App from './App.jsx'
+
+//utilizar el componente
+ReactDOM.render(<App />,
+    document.getElementById("root"))
